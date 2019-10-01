e6e5c089bcbd20df8131aa11bfa2dab397016afe
willow123red <ljwillow@hotmail.com>
2019-10-01 12:25:28 -0600

    add long description to seeds

--- src/db/01_seeds.sql
+++ src/db/01_seeds.sql
@@ -3,9 +3,9 @@ VALUES ('Mickey', 'Mouse', '100 Minnie Street', 'Calgary', 'Alberta', 'Canada', 
 ('Donald', 'Duck', '200 Daisy Street', 'Toronto', 'Ontario', 'Canada', 'donaldduck@mail.com'),
 ('Bruce', 'Wayne', '1 Gotham Rd', 'Gotham City', 'Gotham', 'United States', 'brucewayne@mail.com');
 
-INSERT INTO charities (name, address, city, province, country, description, qr_img_url, created_at)
-VALUES ('Slumdogs', '101 Dalmations Lane', 'Calgary', 'Alberta', 'Canada', 'We look after street dogs and find them new homes!', null, null),
-('Feed me!', '2 Meals on Wheels Road', 'Vancouver', 'British Columbia', 'Canada', 'Helping deliver meals to seniors and disabled clients.', null, null);
+INSERT INTO charities (name, address, city, province, country, short_description, long_description, qr_img_url, created_at)
+VALUES ('Slumdogs', '101 Dalmations Lane', 'Calgary', 'Alberta', 'Canada', 'We look after street dogs and find them new homes!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', null),
+('Feed me!', '2 Meals on Wheels Road', 'Vancouver', 'British Columbia', 'Canada', 'Helping deliver meals to seniors and disabled clients.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', null);
 
 INSERT INTO employees (first_name, last_name, charity_id)
 VALUES ('Indiana', 'Jones', 1),
