6b4bf5b93a5d826e3266891d18cc262f095b6264
willow123red <ljwillow@hotmail.com>
2019-10-01 16:02:57 -0600

    amend env file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(5000);
+app.listen(process.env.PORT);
